40384e75d1ac0c8b64d5f8b0e57c357d1aaf7282
fachry16 <fachryhakeemalm@gmail.com>
2026-09-01 17:46:16 +0700

    refactor: Simplify LmsSidebarComposer

--- app/Http/ViewComposers/LmsSidebarComposer.php
+++ app/Http/ViewComposers/LmsSidebarComposer.php
@@ -22,69 +22,53 @@ class LmsSidebarComposer
         $user = $auth->user();
         $view->with('auth_user', $user);
 
-        // Ambil tahun akademik aktif
         $tahunAkademik = TahunAkademik::where('is_active', true)->first();
         $view->with('tahun_akademik', $tahunAkademik);
 
-        // Tentukan kelas berdasarkan role
         $kelasTerdaftar = collect();
         $pendingSubmissions = 0;
 
-        if ($user->isMahasiswa()) {
-            $mahasiswa = $user->mahasiswa;
-            if ($mahasiswa && $tahunAkademik) {
-                $kelasTerdaftar = $mahasiswa->pengampus()
-                    ->where('pengampus.tahun_akademik_id', $tahunAkademik->id)
-                    ->with(['mataKuliah', 'dosen.user'])
-                    ->get();
-            }
-        } elseif ($user->isKaprodi() || $user->isDosen() || $user->isDirektur()) {
-            $userDosen = null;
-            if ($user->dosen) {
-                $userDosen = $user->dosen;
-            }
+        if ($tahunAkademik) {
+            if ($user->isMahasiswa()) {
+                $mahasiswa = $user->mahasiswa;
+                if ($mahasiswa) {
+                    $kelasTerdaftar = $mahasiswa->pengampus()
+                        ->where('pengampus.tahun_akademik_id', $tahunAkademik->id)
+                        ->with(['mataKuliah', 'dosen.user'])
+                        ->get();
 
-            if ($userDosen && $tahunAkademik) {
-                $kelasTerdaftar = $userDosen->pengampus()
-                    ->where('pengampus.tahun_akademik_id', $tahunAkademik->id)
-                    ->with(['mataKuliah'])
+                    $idTugasDikumpul = LmsSubmission::where('mahasiswa_id', $mahasiswa->id)
+                        ->pluck('lms_tugas_id');
+
+                    $pengampuIds = $kelasTerdaftar->pluck('id');
+                    $allTugas = LmsTugas::whereIn('pengampu_id', $pengampuIds)->get();
+                    $pendingSubmissions = $allTugas->filter(function ($tugas) use ($idTugasDikumpul) {
+                        return !$idTugasDikumpul->contains($tugas->id);
+                    })->count();
+                }
+            } elseif ($user->isDosen() || $user->isKaprodi() || $user->isDirektur() || $user->isAdmin()) {
+                $query = Pengampu::query()->where('tahun_akademik_id', $tahunAkademik->id);
+
+                if ($user->isDosen() || $user->isKaprodi() || $user->isDirektur()) {
+                    $dosen = $user->dosen;
+                    if ($dosen) {
+                        $query->where('dosen_id', $dosen->id);
+                    }
+                }
+
+                $kelasTerdaftar = $query->with(['mataKuliah', 'dosen.user'])
                     ->get();
-            } elseif ($user->role === 'admin') {
-                $pengampuIds = Pengampu::where('tahun_akademik_id', $tahunAkademik->id)->pluck('id');
-                $kelasTerdaftar = Pengampu::whereIn('id', $pengampuIds)
-                    ->with(['mataKuliah', 'dosen.user'])
-                    ->get();
+
+                if ($user->isAdmin() || $user->isDosen() || $user->isKaprodi()) {
+                    $pengampuIds = $kelasTerdaftar->pluck('id');
+                    $pendingSubmissions = LmsSubmission::whereHas('lmsTugas', function ($q) use ($pengampuIds) {
+                        $q->whereIn('pengampu_id', $pengampuIds);
+                    })->whereNull('nilai')->count();
+                }
             }
         }
 
         $view->with('kelas_terdaftar', $kelasTerdaftar);
-
-        // Hitung tugas belum dikumpulkan (only for mahasiswa and admin)
-        if ($user->isMahasiswa() || $user->isAdmin()) {
-            $idTugasDikumpul = collect();
-
-            if ($user->isMahasiswa()) {
-                $mahasiswa = $user->mahasiswa;
-                if ($mahasiswa && $tahunAkademik) {
-                    $idTugasDikumpul = LmsSubmission::where('mahasiswa_id', $mahasiswa->id)
-                        ->pluck('lms_tugas_id');
-                }
-            } elseif ($user->isAdmin() && $tahunAkademik) {
-                $pengampuIds = Pengampu::where('tahun_akademik_id', $tahunAkademik->id)->pluck('id');
-                $idTugasDikumpul = LmsSubmission::whereHas('lmsTugas', function ($q) use ($pengampuIds) {
-                    $q->whereIn('pengampu_id', $pengampuIds);
-                })->whereNull('nilai')->pluck('lms_tugas_id');
-            }
-
-            if ($kelasTerdaftar->count() > 0) {
-                $pengampuIds = $kelasTerdaftar->pluck('id');
-                $allTugas = LmsTugas::whereIn('pengampu_id', $pengampuIds)->get();
-                $pendingSubmissions = $allTugas->filter(function ($tugas) use ($idTugasDikumpul) {
-                    return !$idTugasDikumpul->contains($tugas->id);
-                })->count();
-            }
-        }
-
         $view->with('pending_submissions', $pendingSubmissions);
     }
 }
